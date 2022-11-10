758de9e3b2d9ec359dd9b6c1b5beee89a060474b
George Barros <georgelucib@gmail.com>
2022-11-10 03:02:43 -0300

    Updates

---
+++ precache-manifest.8664654e86cf3a531f8294526d32e4c9.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "4935f59c31b82be4f652",
+    "url": "/Crud-React/static/css/main.e7efd035.chunk.css"
+  },
+  {
+    "revision": "4935f59c31b82be4f652",
+    "url": "/Crud-React/static/js/main.d7322334.chunk.js"
+  },
+  {
+    "revision": "d9e24ddb0fb3b7892a0b",
+    "url": "/Crud-React/static/js/runtime~main.37eb59c9.js"
+  },
+  {
+    "revision": "69f69aaf0470ac531a3b",
+    "url": "/Crud-React/static/css/2.38132059.chunk.css"
+  },
+  {
+    "revision": "69f69aaf0470ac531a3b",
+    "url": "/Crud-React/static/js/2.6d50891d.chunk.js"
+  },
+  {
+    "revision": "1972f29574012851c652",
+    "url": "/Crud-React/static/js/3.1749d0fc.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Crud-React/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Crud-React/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Crud-React/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Crud-React/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Crud-React/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "95dbbdd780e739e5d2240c68e198a9b1",
+    "url": "/Crud-React/static/media/CRUD.95dbbdd7.png"
+  },
+  {
+    "revision": "32c649451e9a3c834323dc5420f7d598",
+    "url": "/Crud-React/index.html"
+  }
+];
